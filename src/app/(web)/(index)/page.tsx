0a580d6fc9202d44f9396877fbffa1795a468890
cm-ayf <cm.ayf2734@gmail.com>
2023-11-15 21:26:38 +0900

    change index design

--- src/app/(web)/(index)/page.tsx
+++ src/app/(web)/(index)/page.tsx
@@ -4,6 +4,7 @@ import { useSuspenseQuery } from "@apollo/client";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 import { useRouter } from "next/navigation";
 import GetGuildsQuery from "./GetGuilds.graphql";
 import GuildCard from "@/components/GuildCard";
@@ -14,6 +15,14 @@ export default function Page() {
 
   return (
     <>
+      <Box sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
+        <Typography variant="h2" sx={{ fontSize: "2em" }}>
+          サーバー
+        </Typography>
+        <Button onClick={() => router.push("/guilds/initiate")}>
+          追加・設定変更
+        </Button>
+      </Box>
       <Grid container spacing={2}>
         {data.user.members.map((member) => (
           <Grid item key={member.guild.id}>
@@ -24,14 +33,6 @@ export default function Page() {
           </Grid>
         ))}
       </Grid>
-      <Box>
-        <Button
-          variant="contained"
-          onClick={() => router.push("/guilds/initiate")}
-        >
-          追加・設定変更
-        </Button>
-      </Box>
     </>
   );
 }
