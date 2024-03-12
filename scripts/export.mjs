570b118a8d419626814fafb2ca926038bc9d3839
cm-ayf <cm.ayf2734@gmail.com>
2024-03-12 13:50:14 +0900

    export/import scripts for db migration

---
+++ scripts/export.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+
+import fs from "fs/promises";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const [users, guilds, members, items, events, displays, receipts, records] =
+  await prisma.$transaction([
+    prisma.user.findMany(),
+    prisma.guild.findMany(),
+    prisma.member.findMany(),
+    prisma.item.findMany(),
+    prisma.event.findMany(),
+    prisma.display.findMany(),
+    prisma.receipt.findMany(),
+    prisma.record.findMany(),
+  ]);
+
+const json = JSON.stringify(
+  { users, guilds, members, items, events, displays, receipts, records },
+  null,
+  2,
+);
+await fs.writeFile("tmp/db.json", json);
---
+++ scripts/import.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+
+import fs from "fs/promises";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const json = await fs.readFile("tmp/db.json", "utf-8");
+const { users, guilds, members, items, events, displays, receipts, records } =
+  JSON.parse(json);
+
+await prisma.$transaction([
+  prisma.user.createMany({ data: users }),
+  prisma.guild.createMany({ data: guilds }),
+  prisma.member.createMany({ data: members }),
+  prisma.item.createMany({ data: items }),
+  prisma.event.createMany({ data: events }),
+  prisma.display.createMany({ data: displays }),
+  prisma.receipt.createMany({ data: receipts }),
+  prisma.record.createMany({ data: records }),
+]);
