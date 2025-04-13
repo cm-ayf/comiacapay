c1d5b0d89fbc08cefdd694a35bced323305bf2a4
cm-ayf <cm.ayf2734@gmail.com>
2025-04-13 20:50:26 +0900

    enable vercel preset

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,9 +1,7 @@
 import type { Config } from "@react-router/dev/config";
-// import vercelPreset from "@vercel/react-router/vite";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   ssr: true,
-  presets: [
-    // vercelPreset(),
-  ],
+  presets: [vercelPreset()],
 } satisfies Config;
