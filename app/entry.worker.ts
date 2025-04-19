47de13afecc9b27845a986716b65156856459bf3
cm-ayf <cm.ayf2734@gmail.com>
2025-04-19 19:39:45 +0900

    caching by service worker

--- app/entry.worker.ts
+++ app/entry.worker.ts
@@ -1,6 +1,38 @@
 /// <reference lib="WebWorker" types="serviceworker" />
+import { CacheFirst, NetworkFirst } from "@remix-pwa/sw/cache";
+import type { DefaultFetchHandlerArgs } from "@remix-pwa/sw/types";
+import "@remix-pwa/worker-runtime";
 
-export {};
+const registerDataCache = new NetworkFirst("registerDataCache", {
+  networkTimeoutInSeconds: 5,
+});
+const assetCache = new CacheFirst("assetCache");
+const manifestCache = new CacheFirst("manifestCache");
+
+export async function defaultFetchHandler({
+  request,
+  context,
+}: DefaultFetchHandlerArgs) {
+  if (request.method !== "GET") return context.fetchFromServer();
+
+  const { pathname } = new URL(request.url);
+
+  if (pathname.endsWith("/register.data")) {
+    return registerDataCache.handleRequest(request);
+  }
+
+  if (process.env["NODE_ENV"] === "production") {
+    if (pathname.startsWith("/assets/")) {
+      return assetCache.handleRequest(request);
+    }
+
+    if (pathname === "/__manifest") {
+      return manifestCache.handleRequest(request);
+    }
+  }
+
+  return context.fetchFromServer();
+}
 
 self.addEventListener<"install">("install", (event) => {
   console.log("Service worker installed");
