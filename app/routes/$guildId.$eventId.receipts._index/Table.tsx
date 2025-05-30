99c16de209627cdcb83532aa5bbd3aa7aaecf93a
cm-ayf <cm.ayf2734@gmail.com>
2025-05-30 16:48:06 +0900

    move DeleteButton to Toolbar

--- app/routes/$guildId.$eventId.receipts._index/Table.tsx
+++ app/routes/$guildId.$eventId.receipts._index/Table.tsx
@@ -1,3 +1,6 @@
+import Delete from "@mui/icons-material/Delete";
+import CircularProgress from "@mui/material/CircularProgress";
+import Tooltip from "@mui/material/Tooltip";
 import { extendTheme, ThemeProvider, useTheme } from "@mui/material/styles";
 import {
   ColumnsPanelTrigger,
@@ -10,25 +13,27 @@ import {
   Toolbar,
   ToolbarButton,
   type GridColDef,
+  type GridRowSelectionModel,
 } from "@mui/x-data-grid";
 import { jaJP } from "@mui/x-data-grid/locales";
-import { useMemo } from "react";
-import { useLoaderData, useRevalidator } from "react-router";
+import { createContext, use, useCallback, useMemo, useState } from "react";
+import { useLoaderData, useParams, useRevalidator } from "react-router";
+import { useFetcher } from "react-router";
 import { useMember } from "../$guildId";
 import { useDisplays } from "../$guildId.$eventId";
 import type { clientLoader } from "./clientLoader";
 import type { IDBReceipt } from "~/lib/idb.client";
 import { Snowflake } from "~/lib/snowflake";
 
-export default function Table(
-  {
-    // selected,
-    // setSelected,
-  }: {
-    selected: string[];
-    setSelected: (ids: string[]) => void;
-  },
-) {
+const DeleteButtonContext = createContext<{
+  receipts: IDBReceipt[];
+  rowSelectionModel: GridRowSelectionModel;
+}>({
+  receipts: [],
+  rowSelectionModel: { type: "include", ids: new Set() },
+});
+
+export default function Table() {
   const { displays } = useDisplays();
   const columns = useMemo<GridColDef[]>(
     () => [
@@ -72,23 +77,26 @@ export default function Table(
   const theme = useTheme();
   const localizedTheme = useMemo(() => extendTheme(theme, jaJP), [theme]);
 
+  const [rowSelectionModel, setRowSelectionModel] =
+    useState<GridRowSelectionModel>({ type: "include", ids: new Set() });
+
   return (
     <ThemeProvider theme={localizedTheme}>
-      <DataGrid
-        loading={revalidator.state === "loading"}
-        showToolbar
-        slots={{ toolbar: TableToolbar }}
-        rows={receipts.map(toRow)}
-        columns={columns}
-        checkboxSelection={me.register}
-        getRowId={(row) => row.id}
-        // rowSelectionModel={selected}
-        // onRowSelectionModelChange={(selected) =>
-        //   setSelected(selected.map(String))
-        // }
-        ignoreValueFormatterDuringExport
-        sx={{ height: "100%" }}
-      />
+      <DeleteButtonContext value={{ receipts, rowSelectionModel }}>
+        <DataGrid
+          loading={revalidator.state === "loading"}
+          showToolbar
+          slots={{ toolbar: TableToolbar }}
+          rows={receipts.map(toRow)}
+          columns={columns}
+          checkboxSelection={me.register}
+          getRowId={(row) => row.id}
+          rowSelectionModel={rowSelectionModel}
+          onRowSelectionModelChange={setRowSelectionModel}
+          ignoreValueFormatterDuringExport
+          sx={{ height: "100%" }}
+        />
+      </DeleteButtonContext>
     </ThemeProvider>
   );
 }
@@ -105,7 +113,59 @@ function TableToolbar() {
       <ExportCsv render={<ToolbarButton />}>
         <GridDownloadIcon fontSize="small" />
       </ExportCsv>
+      <Tooltip title="選択した履歴を削除">
+        <span>
+          <DeleteButton />
+        </span>
+      </Tooltip>
     </Toolbar>
+  );
+}
+
+function DeleteButton() {
+  const { receipts, rowSelectionModel } = use(DeleteButtonContext);
+  const { guildId, eventId } = useParams();
+  const fetcher = useFetcher();
+
+  const searchParams = useMemo(() => {
+    const searchParams = new URLSearchParams();
+    switch (rowSelectionModel.type) {
+      case "include":
+        for (const id of rowSelectionModel.ids) {
+          searchParams.append("id", String(id));
+        }
+        break;
+      case "exclude":
+        for (const receipt of receipts) {
+          if (!rowSelectionModel.ids.has(receipt.id)) {
+            searchParams.append("id", receipt.id);
+          }
+        }
+        break;
+    }
+    return searchParams;
+  }, [rowSelectionModel, receipts]);
+
+  const onClick = useCallback(() => {
+    if (searchParams.size === 0) return;
+
+    fetcher.submit(null, {
+      method: "DELETE",
+      action: `/${guildId}/${eventId}/receipts?${searchParams}`,
+    });
+  }, [fetcher, guildId, eventId, searchParams]);
+
+  return (
+    <ToolbarButton
+      onClick={onClick}
+      disabled={searchParams.size === 0 || fetcher.state !== "idle"}
+    >
+      {fetcher.state === "idle" ? (
+        <Delete fontSize="small" />
+      ) : (
+        <CircularProgress color="inherit" size={20} />
+      )}
+    </ToolbarButton>
   );
 }
 
--- app/routes/$guildId.$eventId.receipts._index/route.tsx
+++ app/routes/$guildId.$eventId.receipts._index/route.tsx
@@ -5,15 +5,7 @@ import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material-pigment-css/Box";
-import Container from "@mui/material-pigment-css/Container";
-import {
-  createContext,
-  Suspense,
-  use,
-  useCallback,
-  useState,
-  type PropsWithChildren,
-} from "react";
+import { Suspense, useCallback, useState } from "react";
 import {
   useFetcher,
   useParams,
@@ -25,114 +17,52 @@ import type { action } from "./action";
 import type { clientLoader } from "./clientLoader";
 import type { loader } from "./loader";
 import { dynamic } from "~/lib/dynamic";
-import type { Handle } from "~/lib/handle";
 
 export { action } from "./action";
 export { clientAction } from "./clientAction";
 export { loader } from "./loader";
 export { clientLoader } from "./clientLoader";
 
-export const handle: Handle<unknown> = {
-  PageContextProvider,
-  TopComponent,
-};
-
-const PageContext = createContext<{
-  tab: "summary" | "table";
-  setTab: (tab: "summary" | "table") => void;
-  selected: string[];
-  setSelected: (selected: string[]) => void;
-}>({
-  tab: "summary",
-  setTab: () => {},
-  selected: [],
-  setSelected: () => {},
-});
-
-function PageContextProvider({ children }: PropsWithChildren) {
-  const [tab, setTab] = useState<"summary" | "table">("summary");
-  const [selected, setSelected] = useState<string[]>([]);
-
-  return (
-    <TabContext value={tab}>
-      <PageContext value={{ tab, setTab, selected, setSelected }}>
-        {children}
-      </PageContext>
-    </TabContext>
-  );
-}
-
-function TopComponent() {
-  const { tab, setTab, selected } = use(PageContext);
-
-  return (
-    <Container
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        "@media (max-width: 600px)": { flexDirection: "column-reverse" },
-      }}
-    >
-      <TabList onChange={(_, value) => setTab(value)}>
-        <Tab label="概要" value="summary" />
-        <Tab label="表" value="table" />
-      </TabList>
-      <Box sx={{ flex: 1 }} />
-      <Buttons selected={tab === "table" ? selected : null} />
-    </Container>
-  );
-}
-
 const Table = dynamic(() => import("./Table"));
 export default function Page() {
-  const { selected, setSelected } = use(PageContext);
+  const [tab, setTab] = useState<"summary" | "table">("summary");
   // TODO: get rid of emotion to have `p: 0` working
   return (
-    <>
+    <TabContext value={tab}>
+      <Box
+        sx={{
+          marginTop: -2,
+          marginInline: -3,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <TabList onChange={(_, value) => setTab(value)}>
+          <Tab label="概要" value="summary" />
+          <Tab label="表" value="table" />
+        </TabList>
+        <Buttons />
+      </Box>
       <TabPanel value="summary" sx={{ p: 0, height: "100%" }}>
         <Summary />
       </TabPanel>
       <TabPanel value="table" sx={{ p: 0, height: "100%" }}>
         <Suspense fallback={<CircularProgress />}>
-          <Table selected={selected} setSelected={setSelected} />
+          <Table />
         </Suspense>
       </TabPanel>
-    </>
+    </TabContext>
   );
 }
 
-function Buttons({ selected }: { selected: string[] | null }) {
+function Buttons() {
   return (
     <Box sx={{ display: "flex", flexDirection: "row", padding: 1, gap: 1 }}>
       <Box sx={{ flex: 1 }} />
-      {selected && <DeleteButton selected={selected} />}
       <PushButton />
       <ReloadButton />
     </Box>
-  );
-}
-
-function DeleteButton({ selected }: { selected: string[] }) {
-  const { guildId, eventId } = useParams();
-  const fetcher = useFetcher();
-
-  const onClick = useCallback(() => {
-    if (selected.length === 0) return;
-    fetcher.submit(null, {
-      method: "DELETE",
-      action: `/${guildId}/${eventId}/receipts?${new URLSearchParams(selected.map((id) => ["id", id]))}`,
-    });
-  }, [fetcher, guildId, eventId, selected]);
-
-  return (
-    <Button
-      variant="contained"
-      onClick={onClick}
-      disabled={selected.length === 0}
-      loading={fetcher.state !== "idle"}
-    >
-      履歴を削除
-    </Button>
   );
 }
 
