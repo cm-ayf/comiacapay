085126b375cb111483ca74adfceeb9e0c30bb702
cm-ayf <cm.ayf2734@gmail.com>
2025-06-14 20:12:07 +0900

    setup playwright

---
+++ playwright.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig, devices } from "@playwright/test";
+
+const { CI } = process.env;
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!CI,
+  retries: CI ? 2 : 0,
+  ...(CI ? { workers: 1 } : {}),
+  reporter: "html",
+  use: {
+    baseURL: "http://localhost:5173",
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "Chrome",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "Safari",
+      use: { ...devices["Desktop Safari"] },
+    },
+
+    {
+      name: "iPhone",
+      use: { ...devices["iPhone SE (3rd gen)"] },
+    },
+    {
+      name: "Pixel",
+      use: { ...devices["Pixel 7"] },
+    },
+  ],
+
+  webServer: {
+    command: "npm start",
+    url: "http://localhost:5173",
+    reuseExistingServer: !CI,
+    timeout: 120000,
+  },
+});
