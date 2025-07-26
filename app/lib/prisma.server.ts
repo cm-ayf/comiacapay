eb344265f900c48dbe11ddb057e2d75b19d4634f
cm-ayf <cm.ayf2734@gmail.com>
2025-07-26 22:59:45 +0900

    error handling via extension

--- app/lib/prisma.server.ts
+++ app/lib/prisma.server.ts
@@ -1,4 +1,6 @@
+import { Prisma, PrismaClient } from "@prisma/client";
 import type { RESTPostOAuth2AccessTokenResult } from "discord-api-types/v10";
+import { data } from "react-router";
 
 // union of one
 export type Discount = SetDiscount;
@@ -18,3 +20,43 @@ declare global {
     type Discounts = Discount[];
   }
 }
+
+function mapKnownError(
+  error: Prisma.PrismaClientKnownRequestError,
+  model: string | undefined,
+) {
+  switch (error.code) {
+    case "P2001": // record does not exist
+    case "P2025": // no record found
+      return data({ code: "NOT_FOUND", model, meta: error.meta }, 404);
+    case "P2002": // unique constraint failed
+    case "P2003": // foreign key constraint failed
+    case "P2014": // would violate required relation
+      return data({ code: "CONFLICT", model, meta: error.meta }, 409);
+    default:
+      console.error(error);
+      return data({ code: "INTERNAL_SERVER_ERROR" }, 500);
+  }
+}
+
+const mapKnownErrorExtension = Prisma.defineExtension({
+  name: "mapKnownError",
+  query: {
+    $allOperations({ model, args, query }) {
+      return query(args).catch((error) => {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+          throw mapKnownError(error, model);
+        } else {
+          console.error(error);
+          throw data({ code: "INTERNAL_SERVER_ERROR" }, 500);
+        }
+      });
+    },
+  },
+});
+
+export function createPrismaClient() {
+  return new PrismaClient().$extends(mapKnownErrorExtension);
+}
+
+export type PrismaClientWithExtensions = ReturnType<typeof createPrismaClient>;
