9b7ec681d50cc7e06659ea0d6b5687b2ab2822a1
cm-ayf <cm.ayf2734@gmail.com>
2025-07-26 23:00:02 +0900

    fix feature flag handling

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,12 +1,6 @@
 import type { Config } from "@react-router/dev/config";
 import { vercelPreset } from "@vercel/react-router/vite";
 
-declare module "react-router" {
-  interface Future {
-    unstable_middleware: true; // 👈 Enable middleware types
-  }
-}
-
 export default {
   ssr: true,
   presets: [vercelPreset()],
