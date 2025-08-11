6b0a80fc77641d303ac4116edf90ce06e19855d5
cm-ayf <cm.ayf2734@gmail.com>
2025-08-11 15:38:59 +0900

    explicit getLoadContext

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,2 +1,8 @@
 import { handleRequest } from "@vercel/react-router/entry.server";
+import { unstable_RouterContextProvider } from "react-router";
+
 export default handleRequest;
+
+export function getLoadContext() {
+  return new unstable_RouterContextProvider();
+}
