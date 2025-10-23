97952697046f29a40f0234e50a53bb8b6332d7c3
cm-ayf <cm.ayf2734@gmail.com>
2025-10-23 09:57:12 +0900

    custom entrypoint

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: "npm start",
+    command: "npm run preview",
     url: "http://localhost:5173",
     reuseExistingServer: !CI,
     timeout: 120000,
