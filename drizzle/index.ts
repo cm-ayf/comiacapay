6157e890992a1a7d9cae0627db3093528f01d1f4
cm-ayf <cm.ayf2734@gmail.com>
2025-12-30 17:04:27 +0900

    migrate scripts

--- drizzle/index.ts
+++ drizzle/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { relations } from "./relations";
+import { relations } from "./relations.ts";
+import * as schema from "./schema.ts";
 
 const client = postgres(process.env["POSTGRES_URL"]!, { prepare: false });
-export const db = drizzle({ relations, client });
+export const db = drizzle({ schema, relations, client });
