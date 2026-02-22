50fbd711bb47eb16fe1d9fabe3c99173ea41fc5a
cm-ayf <cm.ayf2734@gmail.com>
2026-02-22 21:27:10 +0900

    ;

--- initial.sql
+++ initial.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS drizzle.__drizzle_migrations (
   id SERIAL PRIMARY KEY,
   hash text NOT NULL,
   created_at bigint
-)
+);
 
 INSERT INTO drizzle.__drizzle_migrations (id, hash, created_at) VALUES (1, '97f3f85fb87a38a2d771386d40c6ec5ff84be5c802d16d8a80b1e0f5b4c30399', 1767078400000);
