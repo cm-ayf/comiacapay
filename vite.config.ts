2b179c88daaef727d0c0281ab22349df587d5faa
cm-ayf <cm.ayf2734@gmail.com>
2026-02-22 11:37:52 +0900

    refactor out vitePwaOptions

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,49 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA, type VitePWAOptions } from "vite-plugin-pwa";
 import vercelCustomEntrypoint from "./plugins/vercel-custom-entrypoint";
+
+const vitePwaOptions: Partial<VitePWAOptions> = {
+  workbox: {
+    runtimeCaching: [
+      {
+        urlPattern: ({ url }) =>
+          /^\/\d+\/\d+\/register\.data$/.test(url.pathname),
+        handler: "NetworkFirst",
+        options: { networkTimeoutSeconds: 5 },
+      },
+      {
+        urlPattern: ({ url }) =>
+          /^\/\d+\/\d+\/register\.data$/.test(url.pathname),
+        // FIXME: not good to expect body-keyed idempotency with POST request
+        method: "POST",
+        handler: "NetworkOnly",
+        options: {
+          backgroundSync: { name: "register" },
+        },
+      },
+    ],
+    navigateFallback: null,
+  },
+  manifest: {
+    lang: "ja",
+    theme_color: "#1976d2",
+    background_color: "#1976d2",
+    name: "Comiacapay",
+    short_name: "Comiacapay",
+  },
+  registerType: "autoUpdate",
+  devOptions: {
+    enabled: true,
+    suppressWarnings: true,
+  },
+  outDir: "build/client",
+};
 
 export default defineConfig({
   plugins: [
     reactRouter(),
-    VitePWA({
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: ({ url }) =>
-              /^\/\d+\/\d+\/register\.data$/.test(url.pathname),
-            handler: "NetworkFirst",
-            options: { networkTimeoutSeconds: 5 },
-          },
-          {
-            urlPattern: ({ url }) =>
-              /^\/\d+\/\d+\/register\.data$/.test(url.pathname),
-            // FIXME: not good to expect body-keyed idempotency with POST request
-            method: "POST",
-            handler: "NetworkOnly",
-            options: {
-              backgroundSync: { name: "register" },
-            },
-          },
-        ],
-        navigateFallback: null,
-      },
-      manifest: {
-        lang: "ja",
-        theme_color: "#1976d2",
-        background_color: "#1976d2",
-        name: "Comiacapay",
-        short_name: "Comiacapay",
-      },
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-        suppressWarnings: true,
-      },
-      outDir: "build/client",
-    }),
+    VitePWA(vitePwaOptions),
     vercelCustomEntrypoint({ main: "./main.ts" }),
   ],
   resolve: {
