1bfb9b51c7ae70edbfedbcf823f8f0c5475c3e58
cm-ayf <cm.ayf2734@gmail.com>
2026-04-11 14:58:15 +0900

    no vercel-custom-entrypoint

--- main.ts
+++
@@ -1,9 +0,0 @@
-import {
-  createRequestHandler,
-  RouterContextProvider,
-  type ServerBuild,
-} from "react-router";
-import * as build from "virtual:react-router/server-build";
-
-const handler = createRequestHandler(build as ServerBuild);
-export default (req: Request) => handler(req, new RouterContextProvider());
